f022350bf92e9cef47269bab4eebfb634455f778
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-31 14:10:22 +0100

    feat: Add Lombok annotations to Task & TaskDto

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,5 +1,10 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public final class TaskDto {
     private Long id;
     private String title;
