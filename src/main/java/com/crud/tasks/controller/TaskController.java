ac50aa524ba50ffd4b9274608d0e5d2b663bc63d
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-31 15:17:14 +0100

    Task 14.4 @RequestMapping for all methods of TaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -17,18 +17,22 @@ public class TaskController {
         return new ArrayList<>();
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long id){
         return new TaskDto(1L, "Test title", "Test content");
     }
 
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId){
 
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto(1L, "Edited test title", "Edited test content");
     }
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto taskDto){
 
     }
