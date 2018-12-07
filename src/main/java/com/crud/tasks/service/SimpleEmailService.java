80a97a6a54c2fce62f727bd89359b92eb9b10b66
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-12-07 14:25:35 +0100

    Kodilla 19.3 - adding Email service

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,44 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class SimpleEmailService {
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public SimpleEmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public SimpleEmailService() {
+    }
+
+    public void send(Mail mail){
+        log.info("Preparing to send an email...");
+
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+
+            log.info("Message sent.");
+        } catch (MailException e){
+            log.error("Failed to send email.", e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getReceiverEmail());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        return mailMessage;
+    }
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTestSuite.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTestSuite {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmail(){
+        Mail mail = new Mail("test@test.com", "Test subject", "Test message");
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getReceiverEmail());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+
+        simpleEmailService.send(mail);
+
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
