9774d370f502757b9d94e592b45c91b9f562222b
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-01-19 16:08:14 +0100

    Kodilla 24.2 Thymeleaf and mails

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class SimpleEmailService {
     private JavaMailSender javaMailSender;
+    private MailCreatorService mailCreatorService;
 
     @Autowired
-    public SimpleEmailService(JavaMailSender javaMailSender) {
+    public SimpleEmailService(JavaMailSender javaMailSender, MailCreatorService mailCreatorService) {
         this.javaMailSender = javaMailSender;
+        this.mailCreatorService = mailCreatorService;
     }
 
     public SimpleEmailService() {
@@ -25,13 +29,21 @@ public class SimpleEmailService {
         log.info("Preparing to send an email...");
 
         try {
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
 
             log.info("Message sent.");
         } catch (MailException e){
             log.error("Failed to send email.", e.getMessage(), e);
         }
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getReceiverEmail());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 
     private SimpleMailMessage createMailMessage(final Mail mail){
