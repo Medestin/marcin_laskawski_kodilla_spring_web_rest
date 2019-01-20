3a615a96b9effa29f603a1379f58b5799d12dd80
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-01-20 14:52:40 +0100

    Kodilla 24.3 ifs and loops in thymeleaf

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,13 +21,20 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
 
     public String buildTrelloCardEmail(String message){
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello account");
+        functionality.add("Application allows sending tasks to Trello");
+
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8080/");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("goodbye_message", "Kind regards, " + adminConfig.getCompanyName());
-        context.setVariable("company_goal", adminConfig.getCompanyGoal());
+        context.setVariable("show_button", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("is_friend", true);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
