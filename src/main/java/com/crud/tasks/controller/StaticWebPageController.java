85a12e259f89e968f1156912e592fcea1b0ce795
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-01-24 14:31:03 +0100

    addition to Task 24.1 as per mentor request (ref. a981a)

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,7 +1,10 @@
 package com.crud.tasks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -13,6 +16,15 @@ public class StaticWebPageController {
         model.put("variable", "My Thymeleaf variable");
         model.put("one", 1);
         model.put("two", 2);
+        return "index";
+    }
+
+    @GetMapping("/ac")
+        public String index(Map<String, Object> model,
+                            @RequestParam(name = "n1") int one, @RequestParam(name = "n2") int two){
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", one);
+        model.put("two", two);
         return "index";
     }
 
